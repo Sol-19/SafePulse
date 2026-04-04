200ebedf349502585c964ef39df0509271f26cbb
Azure愛 <kenieldeguzman45@gmail.com>
2026-04-04 21:12:05 +0800

    checking net

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,19 +1,34 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import "../global.css";
 import * as SystemUI from 'expo-system-ui';
 import { ImageBackground } from 'react-native'; 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
 
 SystemUI.setBackgroundColorAsync('#3723A9');
 
 
 
 export default function RootLayout() {
+
   
-  
+  const router = useRouter();
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      const connected = state.isConnected && state.isInternetReachable;
+      setIsConnected(connected);
+      if (!connected) {
+      router.replace("/offline");}
+      if (connected) {
+      router.replace("/loading");}
+    });
+    return () => unsubscribe();}, []);
 
   return (
 
@@ -38,6 +53,7 @@ export default function RootLayout() {
         <Stack.Screen name="login" options={{headerShown: false}}/>
          <Stack.Screen name="welcome" options={{headerShown: false}}/>
          <Stack.Screen name="loading" options={{headerShown: false}}/>
+         <Stack.Screen name="offline" options={{headerShown: false}}/>
         
       
       </Stack>
